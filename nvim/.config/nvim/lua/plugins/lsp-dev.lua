bb420925518c759705f6ef0dd442c203ffc3b53f
Milos Zivlak <zivlakmilos@gmail.com>
2024-03-21 22:56:14 +0100

    Lsp Dev load plugin

---
+++ nvim/.config/nvim/lua/plugins/lsp-dev.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    name = "educationalsp",
+    dir = "/home/zi/personal/educationalsp/nvim/",
+    lazy = false,
+    fallback = false,
+
+    config = function()
+      require("educationalsp")
+    end,
+  },
+}
