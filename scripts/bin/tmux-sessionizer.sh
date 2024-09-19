aa86bac515c1b064933575c2d7e7ea6616322f7f
Milos Zivlak <zivlakmilos@gmail.com>
2024-09-20 01:14:04 +0200

    Tmux sessionizer update

--- scripts/bin/tmux-sessionizer.sh
+++ scripts/bin/tmux-sessionizer.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-	selected=$1
+  selected=$1
 else
-	selected=$(find ~/personal ~/school ~/work ~/work/dbtsearch ~/work/sensa ~/work/freelance/ ~/work/lyra/ ~/learning/ ~/tmp/ -mindepth 1 -maxdepth 1 -type d | fzf)
+  selected=$(find ~/personal ~/school ~/work ~/work/*/ ~/learning/ ~/tmp/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
-	exit 0
+  exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -ds $selected_name -c $selected
-	tmux rename-window -t $selected_name:1 Code
-	tmux send-keys -t $selected_name:1 nvim Enter
-	tmux new-window -dn Build -c $selected -t $selected_name
+  tmux new-session -ds $selected_name -c $selected
+  tmux rename-window -t $selected_name:1 Code
+  tmux send-keys -t $selected_name:1 nvim Enter
+  tmux new-window -dn Build -c $selected -t $selected_name
 
-	tmux attach-session -dt $selected_name
-	exit 0
+  tmux attach-session -dt $selected_name
+  exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	tmux new-session -ds $selected_name -c $selected
-	tmux rename-window -t $selected_name:1 Code
-	tmux send-keys -t $selected_name:1 nvim Enter
-	tmux new-window -dn Build -c $selected -t $selected_name
+  tmux new-session -ds $selected_name -c $selected
+  tmux rename-window -t $selected_name:1 Code
+  tmux send-keys -t $selected_name:1 nvim Enter
+  tmux new-window -dn Build -c $selected -t $selected_name
 fi
 
 if [[ -z $TMUX ]]; then
-	tmux attach-session -dt $selected_name
-	exit 0
+  tmux attach-session -dt $selected_name
+  exit 0
 fi
 
 tmux switch-client -t $selected_name
