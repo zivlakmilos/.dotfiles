7a12f8d86fb749c0bb6cf85ecc73f73930f8e1f5
Milos Zivlak <zivlakmilos@gmail.com>
2025-06-01 03:58:35 +0200

    Tmux sessionizer allowing local script start script per project

--- scripts/bin/tmux-sessionizer.sh
+++ scripts/bin/tmux-sessionizer.sh
@@ -15,9 +15,12 @@ tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
   tmux new-session -ds $selected_name -c $selected
-  tmux rename-window -t $selected_name:1 Code
-  tmux send-keys -t $selected_name:1 nvim Enter
-  tmux new-window -dn Build -c $selected -t $selected_name
+
+  if [ -f "$selected/.tmux-sessionizer" ]; then
+    tmux send-keys -t "$selected_name" "source $selected/.tmux-sessionizer" c-M
+  elif [ -f "$HOME/.tmux-sessionizer" ]; then
+    tmux send-keys -t "$selected_name" "source $HOME/.tmux-sessionizer" c-M
+  fi
 
   tmux attach-session -dt $selected_name
   exit 0
@@ -25,9 +28,12 @@ fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
   tmux new-session -ds $selected_name -c $selected
-  tmux rename-window -t $selected_name:1 Code
-  tmux send-keys -t $selected_name:1 nvim Enter
-  tmux new-window -dn Build -c $selected -t $selected_name
+
+  if [ -f "$selected/.tmux-sessionizer" ]; then
+    tmux send-keys -t "$selected_name" "source $selected/.tmux-sessionizer" c-M
+  elif [ -f "$HOME/.tmux-sessionizer" ]; then
+    tmux send-keys -t "$selected_name" "source $HOME/.tmux-sessionizer" c-M
+  fi
 fi
 
 if [[ -z $TMUX ]]; then
